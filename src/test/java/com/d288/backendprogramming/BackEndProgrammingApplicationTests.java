0e0f3e7dd4b81df31efacc301378598a71e65db2
Admin <eray@wgu.edu>
2024-08-07 20:03:29 -0700

    initial commit task B.

---
+++ src/test/java/com/d288/backendprogramming/BackEndProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.backendprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
